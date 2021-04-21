467e6df897d8a0958a0ac3c99ff0d139d355f525
Daniel Perez <plupyt_dany@hotmail.com>
2021-04-21 15:51:26 -0500

    Layout agregado a las rutas

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,10 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import Header from '../components/Header';
+import React from 'react';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
-import Footer from '../components/Footer';
 import useInitialState from '../hooks/useInitialState';
 import '../assets/styles/App.scss';
 
@@ -13,10 +11,8 @@ const API = 'http://localhost:3000/initalState';
 const Home = () => {
     const initialState = useInitialState(API);
     return (
-        <div className="App">
-            <Header />
+        <>
             <Search />
-
             {
                 initialState.mylist.length > 0 &&
                 <Categories title="Mi Lista">
@@ -49,9 +45,7 @@ const Home = () => {
                     }
                 </Carousel>
             </Categories >
-
-            <Footer />
-        </div>
+        </>
     );
 }
 
